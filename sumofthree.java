56dd4a36b5cb700fdc289517c2c11bd64daeaee8
cdacchetan <cdacchetan@gmail.com>
2024-10-18 14:43:03 +0530

    Java program to find the sum of smallest and largest numbers

--- sumofthree.java
+++ sumofthree.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FindSum {
+public class sumofthree {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number:");
